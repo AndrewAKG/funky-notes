0188959faa7022373d86d7a8d2f8dabf9ec3b473
AndrewAKG <andrew.akram@callvita.com>
2020-06-19 21:12:32 +0200

    feat: notes count and remove note

--- myscripts.js
+++ myscripts.js
@@ -1,12 +1,16 @@
-const createNote = (noteTitle, noteBody) => {
-  let ul = document.getElementById("notes");
+let notesCount = 0;
 
+let itemList = document.getElementById("notes");
+
+let noNotesEl = document.getElementById("no-notes");
+
+const createNote = (noteTitle, noteBody) => {
   let li = document.createElement("li");
   let a = document.createElement("a");
   let h2 = document.createElement("h2");
   let p = document.createElement("p");
   let xButton = document.createElement("button");
-  
+
   xButton.classList.add("delete");
 
   let xText = document.createTextNode("X");
@@ -23,11 +27,17 @@ const createNote = (noteTitle, noteBody) => {
   a.setAttribute("href", "#");
 
   li.appendChild(a);
-  ul.appendChild(li);
+  itemList.appendChild(li);
 };
 
 const createNoteFromInput = (e) => {
   e.preventDefault();
+  notesCount++;
+
+  if (notesCount > 0) {
+    noNotesEl.classList.add("hidden");
+  }
+
   let titleInput = document.getElementById("new-note-title-input");
   let bodyInput = document.getElementById("new-note-body-input");
 
@@ -45,6 +55,27 @@ const createNoteFromInput = (e) => {
   createNote(noteTitle, noteBody);
 };
 
+const removeNote = (e) => {
+  if (e.target.classList.contains("delete")) {
+    if (
+      confirm(
+        `Are you sure to delete the "${e.target.previousElementSibling.innerText}" note?`
+      )
+    ) {
+      let li = e.target.parentElement.parentElement;
+
+      itemList.removeChild(li);
+      notesCount--;
+
+      if (notesCount < 1) {
+        noNotesEl.classList.remove("hidden");
+      }
+    }
+  }
+};
+
 document
   .getElementById("inputForm")
   .addEventListener("submit", createNoteFromInput, false);
+
+itemList.addEventListener("click", removeNote);
